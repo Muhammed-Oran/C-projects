579d65be353a74f167f228259c1521aec32e6479
Muhammed-Oran <76443954+Muhammed-Oran@users.noreply.github.com>
2021-01-14 20:50:14 +0300

    Rename fibonacci 2.c to Calculate Fibonacci Series With Loop.c

--- Calculate Fibonacci Series With Loop.c
+++ Calculate Fibonacci Series With Loop.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	// fibonacci serisinin ilk 10 sayısı
+	// fibonacci serisinin ilk 10 sayÃ½sÃ½
 int a,b,c,d,i;
 a = 1;
 b = 1;
