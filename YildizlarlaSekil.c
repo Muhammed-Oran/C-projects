644edb24ce3237925644d9a9eb54966a6c765a27
Muhammed-Oran <76443954+Muhammed-Oran@users.noreply.github.com>
2021-01-14 19:02:16 +0300

    Add files via upload

---
+++ YildizlarlaSekil.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i,j,g;
+	printf("Giris:");
+	scanf("%d",&g);
+	for(i=1;i<=g;i++)
+	{
+	 for(j=1;j<=i;j++)
+	 {
+	 	printf("*");
+	 }
+		 printf("\n");
+	
+	}
+	
+	
+	return 0;
+}
