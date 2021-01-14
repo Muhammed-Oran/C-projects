3671031f3531e36031117b2329bb7aeeccd91471
Muhammed-Oran <76443954+Muhammed-Oran@users.noreply.github.com>
2021-01-14 20:44:21 +0300

    Update Domdis (Guessing Game).c

--- Domdis (Guessing Game).c
+++ Domdis (Guessing Game).c
@@ -3,22 +3,36 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main() {
 	
 	char a[23];
 	int b,i;
+	i=5; // Right to try
 	printf("Your Name:");
 	scanf("%s",a);
-	printf("\n-Hello %s welcome . I am Domdis Can you guess the number in my mind ?\n\n-Tip = The number in my mind between 1 and 10\n\n-Warning:You Only Have 5 right",a);
+	printf("\n-Hello %s welcome . I am Domdis Can you guess the number in my mind ?\n\n",a);
+	printf("-Tip = The number in my mind between 1 and 10");
+	printf("\n\n-Warning:You Only Have %d right",i);
 	printf("\n\n\nEnter Your guess :");
 	scanf("%d",&b);
-	for(i=1;i<=4;i++) {
+	for(i=i-1;i>=1;i--) {
       if(b != 2)
 	{
-		printf("\nThe number in my mind isn't %d\n",b);
+		printf("\nThe number in my mind isn't %d         (You have %d attempts left)\n\n",b,i);
 		printf("Please Try Again:");
 		scanf("%d",&b);
-	
+	if(i==1)
+	{
+		if(b!=2)
+		{
+				printf("\nThe number in my mind isn't %d",b);
+		}
+		else
+		{
+				printf("\n**The number in my mind was %d . You found it ,Domdis congratulate you.**",b);
+				return 0;
+		}
+	}
 	
 	}
 	
@@ -30,6 +44,10 @@ int main(int argc, char *argv[]) {
 	 }
  
   printf("\n\n         --Your Rights Are Over and Domdis is Very Upset Because you did'nt Guess Correctly--\n\n");
+	
+	
+	
+	
 
 	
 	return 0;
