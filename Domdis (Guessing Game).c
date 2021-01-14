c64f426e4b78bb4e4a4ddb91642c6916ae71b0ea
Muhammed-Oran <76443954+Muhammed-Oran@users.noreply.github.com>
2021-01-14 19:58:02 +0300

    Update Domdis (Guessing Game).c

--- Domdis (Guessing Game).c
+++ Domdis (Guessing Game).c
@@ -5,32 +5,32 @@
 
 int main(int argc, char *argv[]) {
 	
-	char a[23];
-	int b;
+	 char a[23];
+	int b,i;
 	printf("Your Name:");
 	scanf("%s",a);
-	printf("\n-Hello %s welcome . I am Domdis Can you guess the number in my mind ?\n\n-Tip = The number in my mind between 1 and 10",a);
+	printf("\n-Hello %s welcome . I am Domdis Can you guess the number in my mind ?\n\n-Tip = The number in my mind between 1 and 10\n\n-Warning:You Only Have 5 right",a);
 	printf("\n\n\nEnter Your guess :");
 	scanf("%d",&b);
+	for(i=1;i<=4;i++) {
       if(b != 2)
 	{
-		printf("\nThe number in my mind isn't %d ",b);
+		printf("\nThe number in my mind isn't %d\n",b);
+		printf("Please Try Again:");
+		scanf("%d",&b);
+	
 	
 	}
 	
 	else
 	{
-		printf("\nThe number in my mind was %d . You found it ,Domdis congratulate you.",b);
-			
+		printf("\n**The number in my mind was %d . You found it ,Domdis congratulate you.**",b);
+		return 0;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+	 }
+ 
+  printf("\n\n         --Your Rights Are Over and Domdis is Very Upset Because you did'nt Guess Correctly--\n\n");
+
 	
 	return 0;
 }
