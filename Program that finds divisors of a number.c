225cbd1c3fd1ba87996adfe42242264448ecee65
Muhammed-Oran <76443954+Muhammed-Oran@users.noreply.github.com>
2021-01-20 20:52:50 +0300

    Update and rename Sayinin Tam bölenlerini bulan program.c to Program that finds divisors of a number.c

--- Program that finds divisors of a number.c
+++ Program that finds divisors of a number.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Sayinin Tam bölenlerini bulan program
-int main(int argc, char *argv[]) {
+// Sayinin Tam bÃ¶lenlerini bulan program
+int main() {
 	int i,a;
 	printf("Sayi:");
 	scanf("%d",&a);
-	printf("\n%d Sayisinin Tam Sayi Bolenleri \n\n",a);
+	printf("\nDivisors of %d\n\n",a);
 	for(i=1;i<=a;i++)
 	{
 		if(a%i==0)
